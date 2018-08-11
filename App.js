1f97e75e4da0a4df5b2dbd8750a57f88d3bff30d
Ryan Casey <Ryan.c4@me.com>
2018-08-11 13:34:37 -0300

    fixed module import

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {GenericButton} from './components/Setup/GenericButton';
+import GenericButton from './components/Setup/GenericButton';
 
 export default class App extends React.Component {
   render() {
@@ -9,12 +8,3 @@ export default class App extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
