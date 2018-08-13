6c4bd7f2d18a21aab773a68c6ba016341e62d367
Geoff Walker <34051140+GeoffWalker@users.noreply.github.com>
2018-08-13 19:20:46 -0300

    Made the generic button an actual button... kind of'

--- components/Setup/GenericButton.js
+++ components/Setup/GenericButton.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, Text, View, Button} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class GenericButton extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>It WORKS! :)</Text>
+              <Button
+              icon={
+                <Icon
+                name='chevron-right'
+                size={15}
+                color='black'/>
+              }
+              title = 'Start Game'
+              />
             </View>
         )
     }
@@ -13,7 +22,7 @@ export default class GenericButton extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: .1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
